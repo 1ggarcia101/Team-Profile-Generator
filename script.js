ff661f3aa92ac8a68d4b9e300ec0e6b79994b17f
Gerardo Garcia <1ggarcia101@gmail.com>
2022-10-08 15:33:01 -0700

    changes to html and js

--- script.js
+++ script.js
@@ -1,88 +1,191 @@
-const Manager = require("./lib/Manager");
-const Engineer = require('./lib/Engineer');
-const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
-const path = require("path");
 const fs = require("fs");
-const OUTPUT_DIR = path.resolve(__dirname, "output")
-const outputPath = path.join(OUTPUT_DIR, "team.html");
-const render = require("./lib/htmlRenderer");
+
+const Engineer = require("./src/Engineer.js");
+const Manager = require("./src/Manager.js");
+const Intern = require("./src/Intern.js");
+
+let employeeArray = [];
 
 
-async function askQuestions(employeeList) {
-    const baseQuestions = [
-        'What is the employee name?',
-        'What is the employee id?',
-        'What is the email of the employee?',
-        'What is the role of the employee?'
-    ];
-    try {
-        const questions = baseQuestions.map(q=> ({ name: q, type: 'input' }));
-        const answers = await inquirer.prompt(questions);
-        console.log(answers['What is the role of the employee?']);
-        if (answers['What is the role of the employee?'].toLowerCase() === 'intern') {
-            const schoolanswer = await inquirer.prompt({name: "What is the name of the student's school?", type: 'input'});
-            const name = answers['What is the employee name?'];
-            const id = answers['What is the employee id?'];
-            const email = answers['What is the email of the employee?'];
-            const school = schoolanswer["What is the name of the student's school?"];
-            const int = new Intern(name, id, email, school);
-            employeeList.push(int);
+const startMenu = () => {
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                name: 'employee_selection',
+                message: "Choose what type of Employee you're adding from the list:",
+                choices: [
+                    "Manager",
+                    "Engineer",
+                    "Intern",
+                    "Quit",
+                ]
+            }
+        ])
+        .then((answers) => {
+            if (answers.employee_selection == "Manager") {
+                addManagertoArray();
+            }
+            else if (answers.employee_selection == "Engineer") {
+                addEngineertoArray();
+            }
+            else if (answers.employee_selection == "Intern") {
+                addInterntoArray();
+            }
+            else {
+                compileTeam();
+                console.log("Thank you for using the application. If you added a team member, the index.html file can be found in the dist folder.")
+                return;
+            }
+        })
+}
+
+const addManagertoArray = () => {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "What is the manager's name?",
+            },
+
+            {
+                type: "input",
+                name: "Id",
+                message: "What is the manager's ID?",
+            },
+
+            {
+                type: "input",
+                name: "email",
+                message: "What is the manager's email?",
+            },
+
+            {
+                type: "input",
+                name: "office_number",
+                message: "What is the manager's office number?",
+            },
+        ])
+        .then((answers) => {
+            const manager = new Manager(answers.name, answers.Id, answers.email, answers.office_number);
+            employeeArray.push(manager);
+            console.log("A manager has been added with the following information:");
+            console.log("Name: " + manager.name+ ", ID: " + manager.Id + ", Email: " + manager.email + ", Office Number: " + manager.officeNumber);
+            startMenu();
         }
-        if (answers['What is the role of the employee?'].toLowerCase() === 'manager') {
-            const officeanswer = await inquirer.prompt({name: "What is the office number?", type: 'input'});
-            const name = answers['What is the employee name?'];
-            const id = answers['What is the employee id?'];
-            const email = answers['What is the email of the employee?'];
-            const office = officeanswer["What is the office number?"];
-            const man = new Manager(name, id, email, office);
-            employeeList.push(man);
+        )
+}
+
+const addEngineertoArray = () => {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "What is the engineer's name?",
+            },
+
+            {
+                type: "input",
+                name: "Id",
+                message: "What is the engineer's ID?",
+            },
+
+            {
+                type: "input",
+                name: "email",
+                message: "What is the engineer's email?",
+            },
+
+            {
+                type: "input",
+                name: "github",
+                message: "What is the engineer's github username?",
+            },
+        ])
+        .then((answers) => {
+            const engineer = new Engineer(answers.name, answers.Id, answers.email, answers.github);
+            employeeArray.push(engineer);
+            console.log("An engineer has been added with the following information:");
+            console.log("Name: " + engineer.name+ ", ID: " + engineer.Id + ", Email: " + engineer.email + ", Github: " + engineer.github);
+            startMenu();
         }
-        if (answers['What is the role of the employee?'].toLowerCase() === 'engineer') {
-            const githubAnswer = await inquirer.prompt({name: "What is engineer's github screen name?", type: 'input'});
-            const name = answers['What is the employee name?'];
-            const id = answers['What is the employee id?'];
-            const email = answers['What is the email of the employee?'];
-            const github = githubAnswer["What is engineer's github screen name?"];
-            const eng = new Engineer(name, id, email, github);
-            employeeList.push(eng);
+        )
+}
+
+const addInterntoArray = () => {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "What is the intern's name?",
+            },
+
+            {
+                type: "input",
+                name: "Id",
+                message: "What is the intern's ID?",
+            },
+
+            {
+                type: "input",
+                name: "email",
+                message: "What is the intern's email?",
+            },
+
+            {
+                type: "input",
+                name: "school",
+                message: "What is the intern's school?",
+            },
+        ])
+        .then((answers) => {
+            const intern = new Intern(answers.name, answers.Id, answers.email, answers.school);
+            employeeArray.push(intern);
+            console.log("An intern has been added with the following information:");
+            console.log("Name: " + intern.name+ ", ID: " + intern.Id + ", Email: " + intern.email + ", School: " + intern.school);
+            startMenu();
         }
-        return employeeList;   
-    } catch(err) {
-    console.log('Error prompting user questions', err);
+        )
+}
+
+const compileTeam = () => {
+    let team = [];
+    for (let i = 0; i < employeeArray.length; i++) {
+        if (employeeArray[i].getRole() == "Manager") {
+            const managerCard = addManagerCard(employeeArray[i]);
+            team.push(managerCard);
+        } else if (employeeArray[i].getRole() == "Engineer") {
+            const engineerCard = addEngineerCard(employeeArray[i]);
+            team.push(engineerCard);
+        } else {
+            const internCard = addInternCard(employeeArray[i]);
+            team.push(internCard);
+        }
     }
+    fs.writeFileSync("./dist/index.html", generateHTML(team.join("")));
+    fs.copyFile("./template_pages/style.css","./dist/style.css", null, () => {});
 }
 
-
-function writeToFile(fileName, data) {
-    fs.appendFile(fileName, data, function (err) {
-    if (err) throw err;
-    console.log('Saved!');
-    });
+const addManagerCard = (manager) => {
+    return `
+            <div class="col">
+                <div class="employee-card">
+                <div class="card-banner">
+                    <h2>${manager.name}</h2>
+                    <h3><i class="fa-solid fa-person-chalkboard"></i> Manager</h3>
+                </div>
+                <div class="card-info">
+                    <h4>ID: ${manager.Id}</h4>
+                    <h4>Email: <a href="mailto: ${manager.email}">${manager.email}</a></h4>
+                    <h4>Office Number: ${manager.officeNumber}</h4>
+                </div>
+                </div>
+            </div>
+    `
 }
 
-
-async function init() {
-    let employeeList = [];
-    const numberOfEmployees = 1;
-
-    try {
-        for (let i=0; i<numberOfEmployees; i++) {
-            employeeList = await askQuestions(employeeList);
-        }
-    
-    } catch(err) {
-        console.log('error:',err);
-    }
-
-    const htmlData = render(employeeList);
-    writeToFile(outputPath, htmlData);
-    
-
-
-
-
-}
-
-
-init();
+startMenu();
